2ca3de4abe0282eca5f347ab573b48f4df6c20a8
Kerry McKenney <kerry.mcke@gmail.com>
2025-11-02 19:57:45 -0500

    trying

--- projects.js
+++ projects.js
@@ -1,14 +1,6 @@
 "use strict";
 
-const express = require("express");
-const app = express();
 
-const port = 8080;
-
-app.use(express.static("./public"));
-
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
 
 // fetch('https://api.github.com/user', {
 //   headers: {
