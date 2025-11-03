29da5ebf5b5f951bfbf2f8df544cc67766fae4b0
Kerry McKenney <kerry.mcke@gmail.com>
2025-11-02 19:39:52 -0500

    edit

--- projects.js
+++ projects.js
@@ -1,9 +1,5 @@
 "use strict";
 
-import { Octokit} from "octokit";
-const octokit = new Octokit({ 
-  auth: CLIENT_ID,
-});
 
 fetch('https://api.github.com/user', {
   headers: {
@@ -11,8 +7,10 @@ fetch('https://api.github.com/user', {
   }
 })
 
+const ghUserName = "kerryoke";
+
 async function getHangmanGameRepo(ghUserName) {
-    const ghUserName = "kerryoke";
+    
     try {
         const res = await fetch(`https://api.github.com/users/${ghUserName}/HangmanGame`)
         const data = await res.json();
